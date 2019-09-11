47deeecbcaba57b173f3e66fc82994d5604d165b
Hans Martin <hansmhank@gmail.com>
2019-09-11 11:27:48 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'jeopardy';
+  title: string = 'jeopardy';
+  age: number = 27;
+
+  constructor() {
+    this.title = 'Jeopardy';
+    this.changeAge(30);
+  }
+
+  changeAge(age: number): void {
+    this.age = age;
+  }
 }
